59ced7cdcabd0b5794706266c076cbb1063f11b4
rhaegar4815 <ezequielmartinez.90@gmail.com>
2015-09-25 12:14:17 -0700

    Unit testing, Package validation
    Added unit tests for scenarios:
    Package parse success
    Package parse failure
    Package validation success
    Package validation failure

--- code/PluralSight.PackageInstaller/Program.cs
+++ code/PluralSight.PackageInstaller/Program.cs
@@ -1,20 +1,41 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace PluralSight.PackageInstaller
 {
-    class Program
+    public class Program
     {
+        private static List<string> _iteratedPackages = new List<string>();
+
         static void Main(string[] args)
         {
+            Console.WriteLine("Parsing package input...");
             var packages = ParsePackages(args);
+
+            try
+            {
+                Console.WriteLine("Validating package dependencies...");
+                var output = ValidatePackages(packages);
+                Console.WriteLine("Validation passed.");
+                Console.Write("Output: {0}", output);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Validation failed;");
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                Console.ReadLine();
+            }
         }
 
         public static IEnumerable<Tuple<string, string>> ParsePackages(string[] packages)
         {
+            if(packages == null)
+                throw new ArgumentNullException();
+
             var lstPackages = new List<Tuple<string, string>>();
             foreach (var package in packages)
             {
@@ -22,11 +43,63 @@ namespace PluralSight.PackageInstaller
                 var packageDependency = package.Substring(package.IndexOf(":") + 1, package.Length - (package.IndexOf(":") + 1));
 
                 packageName = packageName.Replace("\r\n", string.Empty);
+                packageDependency = packageDependency.Trim();
 
                 lstPackages.Add(new Tuple<string, string>(packageName, packageDependency));
             }
 
             return lstPackages;
         }
+
+        public static string ValidatePackages(IEnumerable<Tuple<string, string>> packages)
+        {
+            _iteratedPackages = new List<string>();
+            
+            var lstPkgInstallOrder = new List<string>();
+            foreach (var package in packages)
+            {
+                if (lstPkgInstallOrder.Contains(package.Item1)) continue;
+                if (String.IsNullOrWhiteSpace(package.Item2) || lstPkgInstallOrder.Contains(package.Item2))
+                {
+                    lstPkgInstallOrder.Add(package.Item1);
+                    continue;
+                }
+
+                var dependency = GetPackageDependency(packages, package.Item2);
+                var requiredDependencies = dependency.Split(',').Where(d => !lstPkgInstallOrder.Contains(d));
+                
+                lstPkgInstallOrder.AddRange(requiredDependencies);
+                lstPkgInstallOrder.Add(package.Item1);
+            }
+
+            return String.Join(", ", lstPkgInstallOrder);
+        }
+
+        private static string GetPackageDependency(IEnumerable<Tuple<string, string>> packages, string dependency)
+        {
+            var package = packages.FirstOrDefault(p => p.Item1 == dependency);
+            if (package == null)
+                throw new PackageInputMissingException();
+
+            if(_iteratedPackages.Contains(dependency))
+                throw new PackageInputLoopException();
+            _iteratedPackages.Add(dependency);
+
+            if (String.IsNullOrWhiteSpace(package.Item2))
+                return package.Item1;
+
+            var packageDependency = GetPackageDependency(packages, package.Item2);
+            return String.Join(",", packageDependency, dependency);
+        }
+    }
+
+    public class PackageInputMissingException : Exception
+    {
+        public PackageInputMissingException() : base("Package input is missing") { }
+    }
+
+    public class PackageInputLoopException : Exception
+    {
+        public PackageInputLoopException() : base("Package dependency loop") { }
     }
 }
---
+++ code/PluralSight.UnitTest/PackageIntallerTests.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace PluralSight.UnitTest
+{
+    [TestClass]
+    public class PackageIntallerTests
+    {
+        private readonly string[] _validInput =
+        {
+            "KittenService:",
+            "Leetmeme: Cyberportal",
+            "Cyberportal: Ice",
+            "CamelCaser: KittenService",
+            "Fraudstream: Leetmeme",
+            "Ice:"
+        };
+
+        private readonly string[] _invalidInput =
+        {
+            "KittenService:",
+            "Leetmeme: Cyberportal",
+            "Cyberportal: Ice",
+            "CamelCaser: KittenService",
+            "Fraudstream:",
+            "Ice: Leetmeme"
+        };
+
+        private readonly string[] _missingInput =
+        {
+            "KittenService:",
+            "Leetmeme: Cyberportal",
+            "Cyberportal: Ice",
+            "CamelCaser: KittenService",
+            "Fraudstream:"
+        };
+
+        private const string VALID_OUTPUT = "KittenService, Ice, Cyberportal, Leetmeme, CamelCaser, Fraudstream";
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void ParsePackageFailure()
+        {
+            var packages = PackageInstaller.Program.ParsePackages(null);
+        }
+
+        [TestMethod]
+        public void ParsePackageSuccess()
+        {
+            var packages = PackageInstaller.Program.ParsePackages(_validInput);
+            
+            Assert.IsNotNull(packages);
+            Assert.AreEqual(packages.Count(), 6);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof (PackageInstaller.PackageInputMissingException))]
+        public void PackageValidationMissingFailure()
+        {
+            var packages = PackageInstaller.Program.ParsePackages(_missingInput);
+            var output = PackageInstaller.Program.ValidatePackages(packages);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof (PackageInstaller.PackageInputLoopException))]
+        public void PackageValidationLoopFailure()
+        {
+            var packages = PackageInstaller.Program.ParsePackages(_invalidInput);
+            var output = PackageInstaller.Program.ValidatePackages(packages);
+        }
+
+        [TestMethod]
+        public void PackageValidationSuccess()
+        {
+            var packages = PackageInstaller.Program.ParsePackages(_validInput);
+            var output = PackageInstaller.Program.ValidatePackages(packages);
+
+            Assert.AreEqual(output, VALID_OUTPUT);
+        }
+    }
+}
